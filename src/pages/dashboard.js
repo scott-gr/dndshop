8228fe0234a3bdb6b90f0f982ddf4aaddd3207e0
Scott Griffin <mscottgriffin@gmail.com>
2020-09-08 23:14:34 -0400

    gatsby page testing

---
+++ src/pages/dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+
+const DashboardPage = () => (
+  <Layout>
+    <h1>Dashboard</h1>
+
+  </Layout>
+)
+export default DashboardPage;
---
+++ src/pages/login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+
+const LoginPage = () => (
+  <Layout>
+    <h1>Login</h1>
+
+  </Layout>
+)
+export default LoginPage;
+
