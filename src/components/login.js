6c6e1a12ab13aa5f7b645723318fa958f5bb7acb
Scott Griffin <mscottgriffin@gmail.com>
2020-09-20 13:23:31 -0400

    connecting to heroku

--- src/components/login.js
+++ src/components/login.js
@@ -1,56 +1,13 @@
-import React from "react"
-import { navigate } from "gatsby"
-import { handleLogin, isLoggedIn } from "../services/auth"
+import React, { Component } from 'react';
 
-class Login extends React.Component {
-  state = {
-    username: ``,
-    password: ``,
-  }
-
-  handleUpdate = event => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    })
-  }
-
-  handleSubmit = event => {
-    event.preventDefault()
-    handleLogin(this.state)
-  }
-
+class login extends Component {
   render() {
-    if (isLoggedIn()) {
-      navigate(`/app/profile`)
-    }
-
     return (
-      <>
-        <h1>Log in</h1>
-        <form
-          method="post"
-          onSubmit={event => {
-            this.handleSubmit(event)
-            navigate(`/app/profile`)
-          }}
-        >
-          <label>
-            Username
-            <input type="text" name="username" onChange={this.handleUpdate} />
-          </label>
-          <label>
-            Password
-            <input
-              type="password"
-              name="password"
-              onChange={this.handleUpdate}
-            />
-          </label>
-          <input type="submit" value="Log In" />
-        </form>
-      </>
-    )
+      <div>
+        
+      </div>
+    );
   }
 }
 
-export default Login
+export default login;
--- src/services/passport.js
+++
@@ -1,5 +0,0 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require ('../models/user.js')
-
-passport.use(new LocalStrategy((username,password)))
