4def5665f1547ca0d4dbac4d4d588cc362444fbe
Scott Griffin <mscottgriffin@gmail.com>
2020-09-21 15:18:43 -0400

    itemschema continued

--- src/models/item.js
+++ src/models/item.js
@@ -1,5 +1,97 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const itemSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	category: {
+		type: String,
+		required: true,
+		enum: [
+			"weapon",
+			"armor",
+			"magic item",
+			"tool",
+			"mount/vehicle",
+			"gear",
+			"food and drink",
+			"trade good",
+			"other",
+		],
+	},
+	description: {
+		type: String,
+		required: true,
+		minlength: 2,
+		maxlength: 100,
+	},
+	rarity: {
+		type: String,
+		enum: [
+			"common",
+			"uncommon",
+			"rare",
+			"very rare",
+			"legendary",
+			"artifact",
+			"unknown",
+		],
+		required: function () {
+			return this.category === "magic item";
+		},
+	},
+	ac: {
+		type: Number,
+		required: function () {
+			return this.category === "armor";
+		},
+	},
+	weight: {
+		type: Number,
+		maxlength: 6,
+	},
+	modifier: {
+		required: function () {
+			return this.category === "armor";
+		},
+		ability: {
+			type: String,
+			enum: [
+				"dexterity",
+				"strength",
+				"wisdom",
+				"intelligence",
+				"charisma",
+				"constitution",
+			],
+		},
+		max: {
+			type: Number,
+		},
+  },
+  damage_dice : {
+    required: function () {
+			return this.category === "weapon";
+    },
+    type: String
+  },
+  damage_type : {
+    required: function () {
+			return this.category === "weapon";
+    },
+    type: String,
+    enum: ["piercing", "bludgeoning", "slashing", "acid",  "cold", "fire", "force", "lightning", "necrotic", "poison", "psychic", "radiant", "thunder"],
+  },
+  properties : {
+    required: function () {
+			return this.category === "weapon";
+    },
+    type: String,
+    enum: []
+  }
+});
 
 // name: name,
 // type: type,
@@ -16,32 +108,4 @@ const Schema = mongoose.Schema;
 // speed: speed,
 // carrying_capacity: carrying_capacity
 
-const itemSchema = new Schema({
-  'type':'object',
-  'properties': {
-    name: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        required: true,
-        enum: ['weapon', 'armor', 'magic item', 'tool', 'mount/vehicle', 'gear', 'food and drink', 'trade good', 'other']
-    },
-    desc: {
-      type: String,
-      required: true,
-      minlength: 2,
-      maxlength: 100,
-    },
-    rarity: {
-      type: String,
-      enum: ['common', 'uncommon', 'rare', 'very rare', 'legendary', 'artifact', 'unknown'],
-      required: ['magic item']
-    }
-  },
-  'definitions': {
-    'rarity-requires-magic-item'
-  }
-})
-module.exports = mongoose.model("User", userSchema, "users")
+module.exports = mongoose.model("Item", itemSchema, "Items");
