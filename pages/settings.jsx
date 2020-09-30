9ee5546511b71ad9364553b1b2a327f6cce94097
Scott Griffin <mscottgriffin@gmail.com>
2020-09-30 09:46:53 -0400

    building profile

---
+++ pages/settings.jsx
@@ -0,0 +1,164 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Head from 'next/head';
+import { useCurrentUser } from '../lib/hooks';
+
+const ProfileSection = () => {
+  const [user, { mutate }] = useCurrentUser();
+  const [isUpdating, setIsUpdating] = useState(false);
+  const nameRef = useRef();
+  const charRef = useRef();
+  const profilePictureRef = useRef();
+  const [msg, setMsg] = useState({ message: '', isError: false });
+
+  useEffect(() => {
+    nameRef.current.value = user.name;
+    charRef.current.value = user.character;
+  }, [user]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (isUpdating) return;
+    setIsUpdating(true);
+    const formData = new FormData();
+    if (profilePictureRef.current.files[0]) { formData.append('profilePicture', profilePictureRef.current.files[0]); }
+    formData.append('name', nameRef.current.value);
+    formData.append('character', charRef.current.value);
+    const res = await fetch('/api/user', {
+      method: 'PATCH',
+      body: formData,
+    });
+    if (res.status === 200) {
+      const userData = await res.json();
+      mutate({
+        user: {
+          ...user,
+          ...userData.user,
+        },
+      });
+      setMsg({ message: 'Profile updated' });
+    } else {
+      setMsg({ message: await res.text(), isError: true });
+    }
+  };
+
+  const handleSubmitPasswordChange = async (e) => {
+    e.preventDefault();
+    const body = {
+      oldPassword: e.currentTarget.oldPassword.value,
+      newPassword: e.currentTarget.newPassword.value,
+    };
+    e.currentTarget.oldPassword.value = '';
+    e.currentTarget.newPassword.value = '';
+
+    const res = await fetch('/api/user/password', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    });
+
+    if (res.status === 200) {
+      setMsg({ message: 'Password updated' });
+    } else {
+      setMsg({ message: await res.text(), isError: true });
+    }
+  };
+
+  async function sendVerificationEmail() {
+    await fetch('/api/user/email/verify', {
+      method: 'POST',
+    });
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Settings</title>
+      </Head>
+      <section>
+        <h2>Edit Profile</h2>
+        {msg.message ? <p style={{ color: msg.isError ? 'red' : '#0070f3', textAlign: 'center' }}>{msg.message}</p> : null}
+        <form onSubmit={handleSubmit}>
+          {!user.emailVerified ? (
+            <p>
+              Your email has not been verified.
+              {' '}
+                <a role="button" onClick={sendVerificationEmail}>
+                  Send verification email
+                </a>
+            </p>
+          ) : null}
+          <label htmlFor="name">
+            Name
+            <input
+              required
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Your name"
+              ref={nameRef}
+            />
+          </label>
+          <label htmlFor="character">
+            character
+            <input
+              id="character"
+              name="character"
+              type="text"
+              placeholder="Character Name"
+              ref={charRef}
+            />
+          </label>
+          <label htmlFor="avatar">
+            Profile picture
+            <input
+              type="file"
+              id="avatar"
+              name="avatar"
+              accept="image/png, image/jpeg"
+              ref={profilePictureRef}
+            />
+          </label>
+          <button disabled={isUpdating} type="submit">Save</button>
+        </form>
+        <form onSubmit={handleSubmitPasswordChange}>
+          <label htmlFor="oldpassword">
+            Old Password
+            <input
+              type="password"
+              name="oldPassword"
+              id="oldpassword"
+              required
+            />
+          </label>
+          <label htmlFor="newpassword">
+            New Password
+            <input
+              type="password"
+              name="newPassword"
+              id="newpassword"
+              required
+            />
+          </label>
+          <button type="submit">Change Password</button>
+        </form>
+      </section>
+    </>
+  );
+};
+const SettingPage = () => {
+  const [user] = useCurrentUser();
+  if (!user) {
+    return (
+      <>
+        <p>Please sign in</p>
+      </>
+    );
+  }
+  return (
+    <>
+      <h1>Settings</h1>
+      <ProfileSection />
+    </>
+  );
+};
+export default SettingPage;
