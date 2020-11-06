5b4172bc32ab19d0f3c11e72609c1606eed0f6d6
Scott Griffin <mscottgriffin@gmail.com>
2020-11-06 15:17:46 -0500

    axios

--- server/controllers/items.controller.js
+++ server/controllers/items.controller.js
@@ -1,16 +1,32 @@
 import { Item } from "../models/index.js";
 
 export function findAll (req, res) {
-  Item.find(req.query)
-	.then(items => res.json(items))
-	.catch(err => res.status(422).json(err));
-};
+  Item.find({})
+  .then(items =>  {
+    res.json({
+      error: false,
+      data: items,
+      message: "Successfully retrieved all item data.",
+    });
+  })
+  .catch(err => {
+  res.status(500).json({
+    error: true,
+    data: null,
+    message: "Error retrieving item data.",
+  });
+});
 
 export function create(req, res) {
   Item.create(req.body)
   .then(items => res.json(items))
-	.catch(err => res.status(422).json(err));
-}
+	.catch(err => {
+    res.status(500).json({
+    error: true,
+    data: null,
+    message: "Error adding new item to database.",
+  });
+});
 
 
 
@@ -65,4 +81,4 @@ export function create(req, res) {
 // 				message: "Error creating item",
 // 			})
 // 		);
-// });
+// })
