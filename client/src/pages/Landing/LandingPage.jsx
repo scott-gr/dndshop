3d5c9d36ff0404e00682c2b24c6a9f595012e3dd
Scott Griffin <mscottgriffin@gmail.com>
2020-11-26 14:46:21 -0500

    landingpage layout

--- client/src/pages/Landing/LandingPage.jsx
+++ client/src/pages/Landing/LandingPage.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import Converter from "../../components/Converter/Converter.jsx";
-import "./landingpage.css";
+import styles from "./landingpage.module.css";
 
 const LandingPage = () => {
 	return (
-		<div>
-			<h1>DnD Shop</h1>
-			<p>
-				The app is still in development. Follow the progress on{" "}
-				<a
-					href="https://github.com/scott-gr/dndshop"
-					target="_blank"
-					rel="noreferrer"
-					alt="github page for this project"
-				>
-					GitHub
-				</a>
-			</p>
-
-			<Converter />
-		</div>
+			<div className={styles.container}>
+				<header className={styles.header}>
+					<h1>DnD Shop</h1>
+				</header>
+				<nav>
+					<p>Nav buttons here?</p>
+				</nav>
+				<main>
+					<p>
+						The app is still in development. Follow the progress on{" "}
+						<a
+							href="https://github.com/scott-gr/dndshop"
+							target="_blank"
+							rel="noreferrer"
+							alt="github page for this project"
+						>
+							GitHub
+						</a>
+					</p>
+				</main>
+				<section className={styles.sidebar}>
+					<Converter />
+				</section>
+				<footer>Made by Scott Griffin</footer>
+			</div>
 	);
 };
 
